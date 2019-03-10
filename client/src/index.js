c3fb7382241bf7a477a28c169fafc863645158f5
Tam Kar <tkarssli@gmail.com>
2019-03-10 16:44:54 -0700

    Layout basic client structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './stylesheets/index.css';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    store = configureStore({});
+    
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={ store }/>, root);
+    
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+

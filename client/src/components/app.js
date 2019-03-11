51b1af696488cb1f58d2606af28f3bacf29a3af7
Tam Kar <tkarssli@gmail.com>
2019-03-10 18:42:19 -0700

    Add searchbar component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../stylesheets/app.css';
 
+import Searchbar from './search/searchbar'
 class App extends Component {
   render() {
     return (
           <div className="App">
             <header className="App-header">
+              <Searchbar/>
               <img src={logo} className="App-logo" alt="logo" />
               <p>
                 Edit <code>src/App.js</code> and save to reload.

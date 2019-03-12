cc8f2ed79157250cd8f9494215e2c411877116fb
Tam Kar <tkarssli@gmail.com>
2019-03-12 14:16:45 -0700

    Fix crash on free games

--- client/src/components/game/gameInfo.jsx
+++ client/src/components/game/gameInfo.jsx
@@ -23,7 +23,7 @@ class GameInfo extends React.Component{
                                     <li><div className="recommendations none icon"></div><p></p></li>)}
                 <li>
                     <div className="price icon"></div>
-                    <p>{price_overview.final_formatted}</p>
+                    <p>{price_overview ? price_overview.final_formatted : "Free"}</p>
                 </li>
             </ul>
         );

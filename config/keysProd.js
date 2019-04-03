bb3a55f4acc3635d1229d8bdf5ef6818a1a07dae
Drew <thesimpledev@gmail.com>
2019-04-03 15:25:53 -0700

    Add igdb key to production

--- config/keysProd.js
+++ config/keysProd.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  steamAPIKey: process.env.STEAM_API_KEY
+  steamAPIKey: process.env.STEAM_API_KEY,
+  igdbKey: process.env.IGDB_KEY
 }

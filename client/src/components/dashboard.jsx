17707d5eb7eb71a6eb36244983b39be5549a38f8
Drew <thesimpledev@gmail.com>
2019-04-03 15:47:24 -0700

    Add some styles to dashboard & total time played

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -18,13 +18,20 @@ class Dashboard extends React.Component {
 
 		return (
 			<>
-				<h3>{ name } | { username }</h3>
-				<img src={ avatarUrl } className="dashboard-avatar"/>
-				<p>Total time played: { totalTimePlayed } hours</p>
+				<div class="container dashboard">
+					<header class="dashboard-header clear">
+						<img src={ avatarUrl } className="dashboard-avatar"/>
+						<h3 className="dashboard-name">{ name } | { username }</h3>
+						<p className="dashboard-timePlayed">Total time played: { totalTimePlayed } hours</p>
+					</header>
 
-				{ ownedGames.map(game => (
-					<img src={ game.image_url } alt={`${ game.name } icon`} />
-				))}
+					<section className="dashboard-ownedGames">
+						<h4 className="dashboard-ownedGames-title">Games Owned</h4>
+						{ ownedGames.map(game => (
+							<img src={ game.image_url } alt={`${ game.name } icon`} className="dashboard-ownedGame" />
+						))}
+					</section>
+				</div>
 			</>
 		)
 	}

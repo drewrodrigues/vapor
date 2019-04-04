ef28d719fb6920428b45c658026b8b8dc5102969
Tam Kar <tkarssli@gmail.com>
2019-04-03 19:08:07 -0700

    Change mongodb cluster

--- config/keysDev.js
+++ config/keysDev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://vapor:7O4Z5kZ7axuC2Lb5@cluster0-iwewy.mongodb.net/test?retryWrites=true',
+  mongoURI: "mongodb+srv://vapor:8ZZ3bNX0rBVwoJUe@cluster0-2s9kj.mongodb.net/test?retryWrites=true",
   secretOrKey: 'lp<Ke-yxv!0BO2/@JN8$[jP)Fb-L%u',
   igdbKey: "9de803e52adfe498a4d21456a080090c",
   steamAPIKey: "138845E97356105CA860A2396372660C"

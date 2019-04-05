15794ff6339f90760ed85db3de8b8478e834cbbe
Drew <thesimpledev@gmail.com>
2019-04-04 23:49:39 -0700

    Add basic schema for User and Game association

--- models/User.js
+++ models/User.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose')
 const Schema   = mongoose.Schema
+const Game = require('./Game')
+
+const oneWeekInMiliseconds = 604800000
 
 const UserSchema = new Schema({
+  name: {
+    type: String
+  },
+  ign: {
+    type: String
+  },
   steamId: {
     type: Number,
     required: true
-  }
+  },
+  totalTimePlayed: {
+    type: Number,
+    default: 0
+  },
+  games: [{ type: Schema.Types.ObjectId, ref: 'games' }],
+  updated: { type: Date, default: Date.now - oneWeekInMiliseconds }
 })
 
 module.exports = User = mongoose.model('users', UserSchema)

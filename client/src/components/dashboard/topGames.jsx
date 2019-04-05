f607781ad2590a23eed74fb0e66389d06fdc3f77
Drew <thesimpledev@gmail.com>
2019-04-04 17:31:20 -0700

    Change owned games to top games

--- client/src/components/dashboard/topGames.jsx
+++ client/src/components/dashboard/topGames.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const TopGames = ({ games }) => (
   games ? <>
-      <section className="dashboard-ownedGames">
-        <h4 className="dashboard-ownedGames-title">Games Owned</h4>
+      <section className="dashboard-topGames">
+        <h4 className="dashboard-topGames-title">Top Games</h4>
         { games.map(game => (
           <img src={ game.image_url } alt={`${ game.name } icon`} className="dashboard-ownedGame" />
         ))}

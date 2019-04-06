a86eae4234ab2dd3fc4ad41a7418a728ab7e8aaf
Drew <thesimpledev@gmail.com>
2019-04-05 18:13:48 -0700

    Only update user once per day

--- models/User.js
+++ models/User.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 const Schema   = mongoose.Schema
 const Game = require('./Game')
 
+const daysAgo = n => {
+  let today = new Date()
+  return today.setDate(today.getDate() - n)
+}
+
 const UserSchema = new Schema({
   name: {
     type: String
@@ -14,7 +19,11 @@ const UserSchema = new Schema({
     required: true
   },
   games: [{ type: Schema.Types.ObjectId, ref: 'games' }],
-  updatedDaysAgo: { type: Number, default: 1 }
+  updated: { type: Date, default: daysAgo(7) }
+})
+
+UserSchema.virtual('needsUpdate').get(function() {
+  return this.updated <= daysAgo(1)
 })
 
 module.exports = User = mongoose.model('users', UserSchema)

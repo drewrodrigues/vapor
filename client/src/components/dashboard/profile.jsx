a6694672915b035de7256c3538bc1c51942f382a
Drew <thesimpledev@gmail.com>
2019-04-12 16:39:32 -0700

    Add stat sections vs averages to dashboard

--- client/src/components/dashboard/profile.jsx
+++ client/src/components/dashboard/profile.jsx
@@ -21,26 +21,42 @@ class Profile extends React.Component {
       averageGames,
       averagePlaytime,
       name,
+      percentageAchievementsCompleted,
+      totalAchievements,
       totalTimePlayed,
+      totalGames,
       username
     } = this.props
-
-    const timePlayed = totalTimePlayed != 0 ? <>
-      <p className="dashboard-timePlayed">Total time played: { totalTimePlayed } hours</p>
-    </> : null
 
     return name ? <>
       <header className="dashboard-header clear">
         <img src={ avatarUrl } className="dashboard-avatar"/>
-        <h3 className="dashboard-name">{ name } | { username }</h3>
-        { timePlayed }
-
-        <ul>
-          <li>averageAchievements: { averageAchievements }</li>
-          <li>averageGames: { averageGames }</li>
-          <li>averagePlaytime: { averagePlaytime }</li>
-        </ul>
+        <h3 className="dashboard-username">{ username }</h3>
+        <h3 className="dashboard-name">{ name }</h3>
       </header>
+
+      <section className="dashboard-stats clear">
+        <div className="dashboard-stat">
+          <p className="dashboard-stat-title">Hours Played</p>
+          <span className="dashboard-stat-user orange">{ totalTimePlayed }</span>
+          <span className="dashboard-stat-average">{ averagePlaytime } average</span>
+        </div>
+        <div className="dashboard-stat">
+          <p className="dashboard-stat-title">Games</p>
+          <span className="dashboard-stat-user blue">{ totalGames }</span>
+          <span className="dashboard-stat-average">{ averageGames } average</span>
+        </div>
+        <div className="dashboard-stat">
+          <p className="dashboard-stat-title">Achievements</p>
+          <span className="dashboard-stat-user green">{ totalAchievements }</span>
+          <span className="dashboard-stat-average">{ averageAchievements } average</span>
+        </div>
+        <div className="dashboard-stat">
+          <p className="dashboard-stat-title">Completion</p>
+          <span className="dashboard-stat-user red">{ percentageAchievementsCompleted }%</span>
+          <span className="dashboard-stat-average">overall achievements</span>
+        </div>
+      </section>
     </> : null
   }
 }

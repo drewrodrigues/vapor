b7d61f76bb267baedb5f5c1025cd3e683bc19170
Tam Kar <tkarssli@gmail.com>
2019-04-18 16:54:05 -0700

    New key

--- config/keysDev.js
+++ config/keysDev.js
@@ -3,6 +3,8 @@ module.exports = {
   secretOrKey: 'lp<Ke-yxv!0BO2/@JN8$[jP)Fb-L%u',
   igdbKeyTamir: "9de803e52adfe498a4d21456a080090c", // Tamir - Out for April
   igdbKeyYoni: "d3f14f30a43416cc35d7d984f062cdc1", // Yoni
-  igdbKey: "bac6ba71222529f4ac7832455102798e", // Drew 
+  igdbKeyDrew: "bac6ba71222529f4ac7832455102798e", // Drew 
+  igdbKeyDrew: "bac6ba71222529f4ac7832455102798e", // Drew 
+  igdbKey: "81cad4b6c172855a7b1cc5a36d5c8c56", //Random
   steamAPIKey: "138845E97356105CA860A2396372660C"
 }
